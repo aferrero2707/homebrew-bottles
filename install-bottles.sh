202093f09bdd4b491b8afcbe01e0c8376a7929e7
aferrero2707 <aferrero1975@gmail.com>
2018-11-05 12:22:47 +0100

    [skip ci]

--- install-bottles.sh
+++ install-bottles.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 BASE_URL="https://github.com/aferrero2707/homebrew-bottles/releases/download/continuous"
-BOTTLES="ilmbase--2.2.1.high_sierra.bottle.1.tar.gz openexr--2.2.0_1.high_sierra.bottle.1.tar.gz"
+BOTTLES="fftw--3.3.8.high_sierra.bottle.1.tar.gz ilmbase--2.2.1.high_sierra.bottle.1.tar.gz openexr--2.2.0_1.high_sierra.bottle.1.tar.gz"
 
 for B in $BOTTLES; do
   wget ${BASE_URL}/$B || exit 1
