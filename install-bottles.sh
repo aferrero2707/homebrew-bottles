82d93c0aad766a733e8a008acca0a2fab7d294e4
aferrero2707 <aferrero1975@gmail.com>
2018-11-05 12:15:18 +0100

    [skip ci] create install-bottles.sh

---
+++ install-bottles.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+BASE_URL="https://github.com/aferrero2707/homebrew-bottles/releases/download/continuous"
+BOTTLES="ilmbase--2.2.1.high_sierra.bottle.1.tar.gz openexr--2.2.0_1.high_sierra.bottle.1.tar.gz"
+
+for B in $BOTTLES; do
+  wget ${BASE_URL}/$B || exit 1
+  brew reinstall -f $B || exit 1
+done
