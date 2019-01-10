e47979d792b7a50a35ab14e153783816430e7e10
aferrero2707 <aferrero1975@gmail.com>
2019-01-10 12:31:08 +0100

    Update build-libpng.sh

--- build-libpng.sh
+++ build-libpng.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-brew upgrade --force --verbose --build-bottle Formula/libpng.rb || exit 1
+brew upgrade --force --build-bottle Formula/libpng.rb || exit 1
 brew bottle libpng || exit 1
