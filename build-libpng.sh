5dbc7f8e33a9fcca1f72e3ffac75df6e3fe686b4
aferrero2707 <aferrero1975@gmail.com>
2019-01-10 13:04:38 +0100

    Update build-libpng.sh

--- build-libpng.sh
+++ build-libpng.sh
@@ -2,4 +2,4 @@
 
 brew uninstall --force --ignore-dependencies libpng
 brew install --force --build-bottle Formula/libpng.rb || exit 1
-brew bottle libpng || exit 1
+brew bottle Formula/libpng.rb || exit 1
